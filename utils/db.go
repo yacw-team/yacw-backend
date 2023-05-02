22cd4e27ec0c3aca490d887642d42c91cb2c7028
Kare-Udon <gpg@udon.eu.org>
2023-05-02 16:22:11 +0800

    Update: change database file name

--- utils/db.go
+++ utils/db.go
@@ -9,7 +9,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("tests.db"), &gorm.Config{})
+	DB, err = gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
