303639d6dde3a77adaeae258b122d18a1e3d67b4
qq_51697026 <1946441559@qq.com>
2023-05-03 20:25:08 +0800

    增加promopt中的空格格式

--- models/prompt.go
+++ models/prompt.go
@@ -5,8 +5,8 @@ type Prompt struct {
 	ModelName     string `gorm:"column:modelname" json:"name"`
 	Description   string `json:"description"`
 	Details       string `json:"details"`
-	ExampleInput  string `gorm:"column:exampleinput"json:"exampleInput"`
-	ExampleOutput string `gorm:"column:exampleoutput"json:"exampleOutput"`
+	ExampleInput  string `gorm:"column:exampleinput" json:"exampleInput"`
+	ExampleOutput string `gorm:"column:exampleoutput" json:"exampleOutput"`
 	Prompts       string `json:"content"`
 	Uid           string `json:"-"`
 	Designer      string `json:"-"`
