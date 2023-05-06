35e87001706fff6e0949701d099dca141805454a
“fzuxpc” <2322042107@qq.com>
2023-05-06 21:13:49 +0800

    alter update the newchat api
    update the newchat api with the new api document,add sending the first user message and generate a title by the user's issue

--- controllers/v1/chat/new.go
+++ controllers/v1/chat/new.go
@@ -1,7 +1,10 @@
 package chat
 
 import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/sashabaranov/go-openai"
 	"github.com/yacw-team/yacw/models"
 	"github.com/yacw-team/yacw/utils"
 	"net/http"
@@ -15,45 +18,133 @@ type NewChatRequest struct {
 }
 
 type NewChatResponse struct {
-	ChatId  string  `json:"chatId"`
-	ModelId string  `json:"modelId"`
-	Content Content `json:"content"`
+	ChatId  string             `json:"chatId"`
+	ModelId string             `json:"modelId"`
+	Content ResponseNewContent `json:"content"`
 }
 
 type Content struct {
 	PersonalityId string `json:"personalityId"`
-	PromptsId     string `json:"promptsId"`
-	System        string `json:"system"`
+	User          string `json:"user"`
 }
 
+type ResponseNewContent struct {
+	PersonalityId string `json:"personalityId"`
+	User          string `json:"user"`
+	Assistant     string `json:"assistant"`
+	Title         string `json:"title"`
+}
+
+// 新建对话API，路由/v1/chat/new
 func NewChat(c *gin.Context) {
 	var request NewChatRequest
 	var response NewChatResponse
 	var err = c.BindJSON(&request)
-	var errChange, errChange1 error
-	var max int
+	var errChange1, errPersonality error
+	var max, modelId int
+	var maxMessage, personalityId int
+	var chatConversation models.ChatConversation
+	var systemMessage, userMessage, assistantMessage models.ChatMessage
+	var systemContent string
+	var assistantResponse, title openai.ChatCompletionResponse
+	//获取modelid，转int
+	if request.ModelId == "" {
+		modelId = 0
+	} else {
+		temp, errModelId := strconv.Atoi(request.ModelId)
+		modelId = temp
+		if errModelId != nil {
+			c.String(http.StatusInternalServerError, "转换失败")
+		}
+	}
 	if err == nil {
-		var chatConversation models.ChatConversation
 		uid := utils.EncryptPassword(request.ApiKey)
+		//获取最大的对话id
 		utils.DB.Table("chatconversation").Select("max(id) a").Find(&max)
+		//获取最大的消息id
+		utils.DB.Table("chatmessage").Select("max(id) a").Find(&maxMessage)
 		chatConversation.Id = max + 1
-		chatConversation.SystemPrompt = request.Content.System
 		chatConversation.Uid = uid
-		chatConversation.ModelId, errChange = strconv.Atoi(request.ModelId)
-		chatConversation.PromptId, errChange1 = strconv.Atoi(request.Content.PromptsId)
-		if errChange != nil || errChange1 != nil {
+		chatConversation.ModelId = modelId
+		//人格设置，默认你是个帮手
+		if request.Content.PersonalityId == "" {
+			systemContent = "You are a helper."
+		} else {
+			personalityId, errPersonality = strconv.Atoi(request.Content.PersonalityId)
+			var personality models.Personality
+			utils.DB.Table("personality").Where("id=?", personalityId).Find(&personality)
+			systemContent = personality.Prompts
+		}
+		if errChange1 != nil || errPersonality != nil {
 			c.JSON(http.StatusInternalServerError, "转换失败")
 		} else {
+			systemMessage.Id = maxMessage + 1
+			systemMessage.Content = systemContent
+			systemMessage.ChatId = chatConversation.Id
+			systemMessage.Actor = 0
+			systemMessage.Show = 0
+			userMessage.Id = maxMessage + 2
+			userMessage.Content = request.Content.User
+			userMessage.ChatId = chatConversation.Id
+			userMessage.Actor = 1
+			userMessage.Show = 1
+			assistantMessage.Id = maxMessage + 3
+			assistantMessage.ChatId = chatConversation.Id
+			assistantMessage.Actor = 2
+			assistantMessage.Show = 1
+			//插入系统消息，用户消息，回答的消息
+			utils.DB.Table("chatmessage").Create(&systemMessage)
+			utils.DB.Table("chatmessage").Create(&userMessage)
+			assistantResponse = ChattingWithGPT(request.ApiKey, request.Content.User, systemContent, modelId)
+			assistantMessage.Content = assistantResponse.Choices[0].Message.Content
+			utils.DB.Table("chatmessage").Create(&assistantMessage)
+			titleString := request.Content.User + ";帮我给这句话取个不是病句、简洁的中文标题"
+			title = ChattingWithGPT(request.ApiKey, titleString, systemContent, modelId)
+			chatConversation.Title = title.Choices[0].Message.Content
+			//插入对话
 			utils.DB.Table("chatconversation").Create(&chatConversation)
-			chatConversation.SystemPrompt = request.Content.System
-			response.Content.System = request.Content.System
-			response.Content.PromptsId = request.Content.PromptsId
+			//返回请求体
+			response.Content.Assistant = assistantResponse.Choices[0].Message.Content
+			response.Content.Title = title.Choices[0].Message.Content
+			response.Content.User = request.Content.User
 			response.Content.PersonalityId = request.Content.PersonalityId
 			response.ModelId = request.ModelId
 			response.ChatId = strconv.Itoa(max + 1)
 			c.JSON(http.StatusOK, response)
 		}
+
 	} else {
 		c.String(http.StatusInternalServerError, "数据绑定失败")
 	}
+}
+
+// 调用chatgpt的函数参数有apikey，问题，人格，模型种类
+func ChattingWithGPT(apiKey string, question string, system string, modelId int) openai.ChatCompletionResponse {
+	client := openai.NewClient(apiKey)
+	var model string
+	if modelId == 0 {
+		model = openai.GPT3Dot5Turbo
+	} else if modelId == 1 {
+		model = openai.GPT432K0314
+	}
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: model,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: question,
+				},
+				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: system,
+				},
+			},
+		},
+	)
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+	}
+	return resp
 }
