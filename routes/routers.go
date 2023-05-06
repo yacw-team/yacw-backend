f7dbc0abd78a963140d3e3f4b7bd49f2304f2c3f
qq_51697026 <1946441559@qq.com>
2023-05-06 15:16:27 +0800

    修改路由和中间件的前缀格式

--- routes/routers.go
+++ routes/routers.go
@@ -11,12 +11,12 @@ func SetupRouter() *gin.Engine {
 	r := gin.Default()
 	//添加中间件
 	r.Use(ApiPrefixMiddleware())
-	r.GET("v1/chat/prompts", controllers.GetPromptShop)
-	r.GET("v1/chat/myprompts", controllers.GetMyPrompt)
-	r.POST("v1/chat/prompts", controllers.CreatePrompt)
-	r.DELETE("v1/chat/prompts", controllers.DeletePrompt)
+	r.GET("/v1/chat/prompts", controllers.GetPromptShop)
+	r.GET("/v1/chat/myprompts", controllers.GetMyPrompt)
+	r.POST("/v1/chat/prompts", controllers.CreatePrompt)
+	r.DELETE("/v1/chat/prompts", controllers.DeletePrompt)
 
-	r.POST("v1/translate/translate", controllers.Translate)
+	r.POST("/v1/translate/translate", controllers.Translate)
 
 	return r
 }
@@ -24,9 +24,6 @@ func SetupRouter() *gin.Engine {
 // ApiPrefixMiddleware 中间件添加前缀
 func ApiPrefixMiddleware() gin.HandlerFunc {
 	apiPath := os.Getenv("API_PATH")
-	if apiPath == "" {
-		apiPath = "/ "
-	}
 	return func(c *gin.Context) {
 		c.Request.URL.Path = path.Join(apiPath, c.Request.URL.Path)
 		c.Next()
