c818225aa575c89b53f0317db6d1052c2b295c6a
qq_51697026 <1946441559@qq.com>
2023-05-07 22:13:33 +0800

    添加验证API的中间件

--- routes/routers.go
+++ routes/routers.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/yacw-team/yacw/controllers"
+	"net/http"
 	"os"
 	"path"
 )
@@ -17,8 +18,8 @@ func SetupRouter() *gin.Engine {
 	r.DELETE("/v1/chat/prompts", controllers.DeletePrompt)
 	r.POST("/v1/chat/apiKey", controllers.VerifyApiKey)
 
-	r.POST("/v1/translate/translate", controllers.Translate)
-	r.POST("/v1/chat/chat", controllers.SendMessage)
+	r.POST("/v1/translate/translate", AuthMiddleware(), controllers.Translate)
+	r.POST("/v1/chat/chat", AuthMiddleware(), controllers.SendMessage)
 	return r
 }
 
@@ -31,7 +32,27 @@ func ApiPrefixMiddleware() gin.HandlerFunc {
 	}
 }
 
-//// AuthMiddleware 中间件验证用户输入了API
-//func AuthMiddleware() gin.HandlerFunc {
-//
-//}
+// AuthMiddleware 中间件验证用户输入了apiKey
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var reqBody map[string]interface{}
+
+		// 使用 Gin 的 ShouldBindJSON 方法将请求参数绑定到 map 中
+		if err := c.ShouldBindJSON(&reqBody); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		// 访问 map 中的字段来获取 POST 请求的每个字段
+		apiKey := reqBody["apiKey"].(string)
+
+		if apiKey == "" {
+			c.Redirect(http.StatusMovedPermanently, "/index?error=nokey")
+			return
+		}
+
+		c.Next()
+	}
+}
