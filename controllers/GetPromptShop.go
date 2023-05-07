65a9a8ed58d66554bd4af7153fe70162cd124560
qq_51697026 <1946441559@qq.com>
2023-05-07 16:55:03 +0800

    修改prompt商店按照type类型获取

--- controllers/GetPromptShop.go
+++ controllers/GetPromptShop.go
@@ -10,6 +10,12 @@ import (
 // GetPromptShop 获取prompt商店的内容
 func GetPromptShop(c *gin.Context) {
 	var prompts []models.Prompt
-	utils.DB.Table("prompt").Where("designer = ?", 0).Find(&prompts)
+	//获取类型
+	promptsType, err := c.GetQuery("type")
+	if err {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+	utils.DB.Table("prompt").Where("designer = ? AND type = ?", 0, promptsType).Find(&prompts)
 	c.JSON(http.StatusOK, prompts)
 }
