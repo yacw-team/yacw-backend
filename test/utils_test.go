c9a256317c88061c704466fa5955260f08a6b4f3
“fzuxpc” <2322042107@qq.com>
2023-05-09 23:07:32 +0800

    update add deletechat unit test
    test to delete a chat by id

--- test/utils_test.go
+++ test/utils_test.go
@@ -1,8 +1,15 @@
 package test
 
 import (
+	"bytes"
+	json2 "encoding/json"
 	"github.com/stretchr/testify/assert"
+	"github.com/yacw-team/yacw/controllers/v1/chat"
+	"github.com/yacw-team/yacw/models"
+	"github.com/yacw-team/yacw/routes"
 	"github.com/yacw-team/yacw/utils"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -12,4 +19,24 @@ func TestEncryptPassword(t *testing.T) {
 	expectedOutput := "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8"
 	output := utils.EncryptPassword(input)
 	assert.Equal(t, expectedOutput, output)
+}
+
+func TestDeleteChat(t *testing.T) {
+	utils.InitDB()
+	r := routes.SetupRouter()
+	utils.DB.Table("chatconversation").Create(&models.ChatConversation{Id: 1, Title: "123", Uid: "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", ModelId: 0})
+	utils.DB.Table("chatmessage").Create(&models.ChatMessage{Id: 1, Content: "123", ChatId: 1, Actor: "user", Show: 1})
+	var temp = chat.DeleteRequest{ApiKey: "123", ChatId: "1"}
+	json, err := json2.Marshal(&temp)
+	reader := bytes.NewReader(json)
+	if err == nil {
+		req, _ := http.NewRequest("DELETE", "/v1/chat/chat", reader)
+		resp := httptest.NewRecorder()
+		r.ServeHTTP(resp, req)
+
+		assert.Equal(t, http.StatusOK, resp.Code)
+	} else {
+		assert.Equal(t, 1, 2)
+	}
+
 }
