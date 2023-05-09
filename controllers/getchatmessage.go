664750fe09fad93d24f428666a661c74f0dee2cc
theluckyson <127723850+theluckyson@users.noreply.github.com>
2023-05-09 22:03:43 +0800

    update getchatmessage.go

--- controllers/getchatmessage.go
+++ controllers/getchatmessage.go
@@ -29,20 +29,25 @@ func GetChatMessage(c *gin.Context) {
 	var errRequestGetMessage = c.ShouldBindJSON(&requestGetMessage)
 	var chatMessages []models.ChatMessage
 	i := 0
-	if errRequestGetMessage == nil {
-		if err := utils.DB.Table("chatmessage").Where("chatid = ?", requestGetMessage.ChatId).Order("id ASC").Find(&chatMessages).Error; err == nil {
-			if len(chatMessages) > 0 {
-				responseGetMessage.ChatId = strconv.Itoa(chatMessages[0].ChatId)
-				for ; i < len(chatMessages); i++ {
-					if chatMessages[i].Show == 1 {
-						responseGetMessage.Messages = append(responseGetMessage.Messages, Message{
-							Type:    chatMessages[i].Actor,
-							Content: chatMessages[i].Content,
-						})
+	apiKeyCheck := utils.ApiKeyCheck(requestGetMessage.Apikey)
+	if apiKeyCheck == true {
+		if errRequestGetMessage == nil {
+			if err := utils.DB.Table("chatmessage").Where("chatid = ?", requestGetMessage.ChatId).Order("id ASC").Find(&chatMessages).Error; err == nil {
+				if len(chatMessages) > 0 {
+					responseGetMessage.ChatId = strconv.Itoa(chatMessages[0].ChatId)
+					for ; i < len(chatMessages); i++ {
+						if chatMessages[i].Show == 1 {
+							responseGetMessage.Messages = append(responseGetMessage.Messages, Message{
+								Type:    chatMessages[i].Actor,
+								Content: chatMessages[i].Content,
+							})
+						}
 					}
 				}
 			}
 		}
+		c.JSON(http.StatusOK, responseGetMessage)
+	} else {
+		c.JSON(http.StatusBadRequest, "3004")
 	}
-	c.JSON(http.StatusOK, responseGetMessage)
 }
