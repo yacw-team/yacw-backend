161901a0017e56ede8feb12eba2e556d1aa8969a
“fzuxpc” <2322042107@qq.com>
2023-05-27 20:37:19 +0800

    update add panic handling into getchatid api
    发生恐慌返回错误码2007

--- controllers/Chat/GetChatId.go
+++ controllers/Chat/GetChatId.go
@@ -21,6 +21,12 @@ type ResponseGetChatId struct {
 }
 
 func GetChatId(c *gin.Context) {
+	defer func() {
+		if err := recover(); err != nil {
+			c.JSON(http.StatusInternalServerError, models.ErrCode{ErrCode: "2007"})
+			// 进行适当的处理
+		}
+	}()
 	var requestGetChatId RequestGetChatId
 	var responseGetChatId ResponseGetChatId
 	var errRequestGetChatId = c.ShouldBindJSON(&requestGetChatId)
