cff787c269e54a7c176937bef85731f3a3459661
qq_51697026 <1946441559@qq.com>
2023-05-28 10:23:27 +0800

    在主函数运行检查是否环境变量或者命令行参数具备

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"github.com/yacw-team/yacw/routes"
 	"github.com/yacw-team/yacw/utils"
+	"os"
 )
 
 func main() {
 
+	_, err := utils.GetSalt()
+	if err != nil {
+		os.Exit(1)
+	}
+
 	utils.InitDB()
 
 	r := routes.SetupRouter()
-	err := r.Run()
+	err = r.Run()
 	if err != nil {
 		print("Something went wrong!")
 		panic(err)
