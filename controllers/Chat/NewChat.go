56ab13b3c4b92156b63a5562e808ebc59123f71b
qq_51697026 <1946441559@qq.com>
2023-05-31 11:54:34 +0800

    修改判断openAI的错误捕捉

--- controllers/Chat/NewChat.go
+++ controllers/Chat/NewChat.go
@@ -2,13 +2,11 @@ package Chat
 
 import (
 	"context"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	openai "github.com/sashabaranov/go-openai"
 	"github.com/yacw-team/yacw/controllers"
 	"github.com/yacw-team/yacw/models"
 	"github.com/yacw-team/yacw/utils"
-	"math"
 	"net/http"
 	"strconv"
 )
@@ -33,13 +31,6 @@ type ResponseNewContent struct {
 
 // NewChat 新建对话API，路由/v1/chat/new
 func NewChat(c *gin.Context) {
-	defer func() {
-		if err := recover(); err != nil {
-			fmt.Println(err)
-			c.JSON(http.StatusInternalServerError, models.ErrCode{ErrCode: "2007"})
-			// 进行适当的处理
-		}
-	}()
 	var response NewChatResponse
 	var maxMessage int
 	var chatConversation models.ChatConversation
@@ -59,7 +50,7 @@ func NewChat(c *gin.Context) {
 	apiKey := reqBody["apiKey"].(string)
 	modelStr := reqBody["modelId"].(string)
 	chatId := reqBody["chatId"].(string)
-	personalityId_f := reqBody["content"].(map[string]interface{})["personalityId"].(float64)
+	personalityId_s := reqBody["content"].(map[string]interface{})["personalityId"].(string)
 	user := reqBody["content"].(map[string]interface{})["user"].(string)
 
 	slice := []string{apiKey, modelStr, user}
@@ -74,7 +65,7 @@ func NewChat(c *gin.Context) {
 		return
 	}
 
-	personalityId := int(math.Ceil(personalityId_f))
+	personalityId, _ := strconv.Atoi(personalityId_s)
 
 	if modelId < 0 || modelId > 6 {
 		c.JSON(http.StatusBadRequest, models.ErrCode{ErrCode: "1005"})
@@ -131,23 +122,9 @@ func NewChat(c *gin.Context) {
 	assistantMessage.Show = 1
 	//插入系统消息，用户消息，回答的消息
 
-	assistantResponse, err = ChattingWithGPT(apiKey, user, systemContent, modelId)
-
-	if err != nil {
-		errCode := utils.GPTRequestErrorCode(err)
-		c.JSON(http.StatusInternalServerError, models.ErrCode{ErrCode: errCode})
-		return
-	}
-
-	assistantMessage.Content = assistantResponse.Choices[0].Message.Content
-	utils.DB.Table("chatmessage").Create(&assistantMessage)
+	assistantResponse, openAIerr_title := ChattingWithGPT(apiKey, user, systemContent, modelId)
 	titleString := "帮我根据以下的文本想一个标题（注意直接返回一个标题，我想直接使用，正式一些，字数在4-6个字）：" + user
-	title, err = ChattingWithGPT(apiKey, titleString, systemContent, modelId)
-	if err != nil {
-		errCode := utils.GPTRequestErrorCode(err)
-		c.JSON(http.StatusInternalServerError, models.ErrCode{ErrCode: errCode})
-		return
-	}
+	title, openAIerr_ass := ChattingWithGPT(apiKey, titleString, systemContent, modelId)
 
 	err = utils.DB.Table("chatmessage").Create(&systemMessage).Error
 	if err != nil {
@@ -159,6 +136,28 @@ func NewChat(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, models.ErrCode{ErrCode: "3009"})
 		return
 	}
+
+	if openAIerr_title != nil || openAIerr_ass != nil {
+		var errCode string
+		chatConversation.Title = ""
+		//插入对话
+		err = utils.DB.Table("chatconversation").Create(&chatConversation).Error
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, models.ErrCode{ErrCode: "3009"})
+			return
+		}
+		if openAIerr_title != nil {
+			errCode = utils.GPTRequestErrorCode(openAIerr_title)
+		} else {
+			errCode = utils.GPTRequestErrorCode(openAIerr_ass)
+		}
+
+		c.JSON(http.StatusInternalServerError, models.ErrCode{ErrCode: errCode})
+		return
+	}
+
+	assistantMessage.Content = assistantResponse.Choices[0].Message.Content
+	utils.DB.Table("chatmessage").Create(&assistantMessage)
 
 	chatConversation.Title = title.Choices[0].Message.Content
 	//插入对话
